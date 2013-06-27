db7ff41578e06d96270fe77bd44677f568c0d217
Joe Hanley <jhanley@mit.edu>
2013-06-27 10:53:00 -0400

    Beaker is working. Binding function to dropper

--- beaker.js
+++ beaker.js
@@ -1,31 +1,45 @@
-var pHtoColor = function(pH){
-    var red = 0 
-    var blue = 0
-    if (pH<=5){
-        red = 1
-        blue = 0
+var beaker = function(){
+    
+    var pHtoColor = function(pH){
+        var red = 1
+        var blue = 0
+        if (pH<=5){
+            red = 1
+            blue = 0
+            
+        }
+        else if ( (5<pH) && (pH<9)){
+            blue =(pH-5) /4
+            red = 1-blue
+        }
+        else if (pH>=9){
+            blue = 1
+            red = 0
+        }
+        $('.red').attr('style','opacity:'+red)
+        $('.blue').attr('style','opacity:'+blue)
+    }
+    
+    var setupBeaker = function (div) {
+        $(div).append("<div class= 'dropper'></div><div><img src=rsz_beakertop.png class='beakerTop'>")
+        .append("<img src=rsz_beakercontents.png class='red'>")
+        .append("<img src=rsz_beakercontentsblue.png class='blue'></div>")
         
     }
-    else if ( (5<pH) && (pH<9)){
-        blue =(pH-5) /4
-        red = 1-blue
+   
+    var pH = Model().currentInfo['pH']
+    var setupDropper = function(){
+        $('.dropper').append("<img src = dropper.png onclick = beaker().pHtoColor("+pH+") class = 'dropperPic'>")
     }
-    else if (pH>=9){
-        blue = 1
-        red = 0
-    }
-    $('.red').attr('style','opacity:'+red)
-    $('.blue').attr('style','opacity:'+blue)
-}
-
-var setupBeaker = function (div) {
-    $(div).append("<img src=rsz_beakertop.png class='beakerTop'>")
-    .append("<img src=rsz_beakercontents.png class='red'>")
-    .append("<img src=rsz_beakercontentsblue.png class='blue'>")
+    
+    var exports = {'pHtoColor':pHtoColor, 'setupBeaker':setupBeaker, 'setupDropper':setupDropper}
+    return exports
     
 }
 
 $(document).ready(function(){
     $('.beaker').each(function(){
-        setupBeaker($(this));});
+        beaker().setupBeaker($(this));});
+    $('.dropper').each(function(){
+        beaker().setupDropper($(this));});
 });
